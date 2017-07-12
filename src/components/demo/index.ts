5e2de19bfbcd4797141d2912601f2d51f24da244
Yunseop Kim <yunsub2@gmail.com>
2017-07-12 12:21:34 +0900

    1. first commit

---
+++ src/components/demo/index.ts
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import {
+  Component
+} from 'vue-property-decorator'
+import './index.postcss'
+const template: Function = require<Function>('./index.pug')
+
+@Component({
+  name: 'Demo',
+  template: template()
+})
+
+export default class Demo extends Vue {
+  get haha(): string {
+    return 'hello typescript vue'
+  }
+}
